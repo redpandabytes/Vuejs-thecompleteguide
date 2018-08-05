0891b869db0b4154cec9d8991adda2968bd10ff9
Nathaniel Baulch-Jones <xdnatty@gmail.com>
2018-08-05 23:09:14 +0100

    Passing event objects

--- Using VueJS to Interact with the DOM/app.js
+++ Using VueJS to Interact with the DOM/app.js
@@ -1,11 +1,17 @@
 new Vue({
     el: '#app',
     data: {
-        counter: 0
+        counter: 0,
+        x: 0,
+        y: 0,
     },
     methods: {
         increase() {
             this.counter++;
+        },
+        updateCoordinates(e) {
+            this.x = event.clientX;
+            this.y = event.clientY;
         }
     }
 });

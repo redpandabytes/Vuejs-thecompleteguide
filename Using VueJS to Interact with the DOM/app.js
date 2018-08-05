17c544cafd0047f26d1d6b79ba33e7aab6761c10
Nathaniel Baulch-Jones <xdnatty@gmail.com>
2018-08-05 23:04:18 +0100

    Simple counter incrementing v:on

--- Using VueJS to Interact with the DOM/app.js
+++ Using VueJS to Interact with the DOM/app.js
@@ -1,14 +1,11 @@
 new Vue({
     el: '#app',
     data: {
-        title: "Hello World!",
-        link: "http://google.com",
-        finishedLink: '<a href="http://google.com">Google</a>'
+        counter: 0
     },
     methods: {
-        sayHello(){
-            this.title = "Hello"
-            return this.title;
+        increase() {
+            this.counter++;
         }
     }
 });

3257c115c88a8b3687accd7d3769a9f794026055
Nathaniel Baulch-Jones <xdnatty@gmail.com>
2018-08-05 22:01:28 +0100

    Using v-once directive

--- Using VueJS to Interact with the DOM/app.js
+++ Using VueJS to Interact with the DOM/app.js
@@ -2,9 +2,11 @@ new Vue({
     el: '#app',
     data: {
         title: "Hello World!",
+        link: "http://google.com"
     },
     methods: {
         sayHello(){
+            this.title = "Hello"
             return this.title;
         }
     }

f47c2389dbb59fbbfa41c68cd744ecb9395b3853
Nathaniel Baulch-Jones <xdnatty@gmail.com>
2018-08-06 21:07:22 +0100

    Two way data binding

--- Using VueJS to Interact with the DOM/app.js
+++ Using VueJS to Interact with the DOM/app.js
@@ -1,21 +1,6 @@
 new Vue({
     el: '#app',
     data: {
-        counter: 0,
-        x: 0,
-        y: 0,
-    },
-    methods: {
-        increase(step, event) {
-            this.counter += step;
-            console.dir(event);
-        },
-        updateCoordinates(e) {
-            this.x = event.clientX;
-            this.y = event.clientY;
-        },
-        alertMe() {
-            alert("aaa!");
-        }
+        name: "Nat"
     }
 });
